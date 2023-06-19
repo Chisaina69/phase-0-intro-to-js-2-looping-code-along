6fb1bed2bfbd1bd7f448dd14389aae89bf7d1ed9
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-06-19 09:04:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+  let messages = [];
+  
+ for (let i = 0; i< names.length; i++){
+    messages.push(
+        `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`
+    )
+ }
+ return messages;
+}
+
+function countDown(countDown) {
+    while (countDown >= 0){
+        countDown--
+        console.log(countDown+1)
+    }
+}
+countDown(5)
